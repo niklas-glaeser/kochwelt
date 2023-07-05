bc2017ee22f3d4e0098329d656ed062aaa2f77bc
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2023-07-05 16:30:40 +0200

    test

--- header.js
+++ header.js
@@ -25,7 +25,7 @@ function goMenuCrossi() {
 }
 
 function goMenuTortellini() {
-  window.location.href="/recipe-pages/tortellini-erbsen-auflauf.html"
+  window.location.href="recipe-pages/tortellini-erbsen-auflauf.html"
 }
 
 function goMenuLasagne() {
@@ -33,34 +33,4 @@ function goMenuLasagne() {
 }
 
 
-/* ALTER CODE
-  function Calculate(){
-    let amount1= 500;
-    let amount2= 1;
-    let amount3= 3;
-    let amount4= 1;
-    let amount5= 4;
-    let amount6= 1;
-    let amount7= 2;
-    let amount8= 100;
-    let amount9= 1;
-  
-    let i = +document.getElementById('inputField').value;
-  
-    if(i<1){
-      i = 1;
-      document.getElementById('inputField').value = 1;
-    }
-  
-    document.getElementById('amount1').innerHTML = `${i * amount1 / 4} `;
-    document.getElementById('amount2').innerHTML = `${i * amount2 / 4} `;
-    document.getElementById('amount3').innerHTML = `${i * amount3 / 4} `;
-    document.getElementById('amount4').innerHTML = `${i * amount4 / 4} `;
-    document.getElementById('amount5').innerHTML = `${i * amount5 / 4} `;
-    document.getElementById('amount6').innerHTML = `${i * amount6 / 4} `;
-    document.getElementById('amount7').innerHTML = `${i * amount7 / 4} `;
-    document.getElementById('amount8').innerHTML = `${i * amount8 / 4} `;
-    document.getElementById('amount9').innerHTML = `${i * amount9 / 4} `;
-  }
-  */ 
 
